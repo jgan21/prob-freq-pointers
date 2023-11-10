02da6e0afe15da76e7c562c9d1374740d3e9e339
Jia Rong Gan <jiaronggan21@gmail.com>
2023-11-10 17:48:43 -0600

    Added frequency counter to function

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,24 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+/** Takes in word and letters and it will return true if letters can be constructed to a word */
+
+function canConstructWord(word, letters) {
+  const wordCounter = frequencyCounter(word);
+  const letterCounter = frequencyCounter(letters);
+
+  for (let letter in wordCounter){
+    if (!(letterCounter[letter] >= wordCounter[letter])) {
+      return false;
+    }
+  }
+  return true;
 }
+
+function frequencyCounter(string){
+  const letterContainer = {};
+  for (let char of string){
+    letterContainer[char] = letterContainer[char] + 1 || 1;
+    }
+  return letterContainer;
+  }
