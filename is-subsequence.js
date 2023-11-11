2090a1afa67663f14878333580da3634ba028b3e
Max Shpungin <maxsdance@gmail.com>
2023-11-10 17:30:50 -0800

    finally subsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -5,23 +5,30 @@
  *  is found if searchString, else returns false
  */
 function isSubsequence(subString,searchString) {
-  let left = 0;
-  let right = 0;
+  //need the order too
+  const subArray = subString.split('');
+  const searchArray = searchString.split('');
+  const subStringCharCount = letterFreq(subString);
+  const searchStringCharCount = letterFreq(searchString);
 
-  while(right < searchString.length){ // while(subString.length < searchString[left])
-    if(subString[left] !== searchString[right]){
-      right++;
-    }else{
-      left++;
-      right++;
+  for(let key of subString){
+    if(!(searchStringCharCount[key] >= subStringCharCount[key])){
+      return false;
     }
-    // if(right === searchString.length-1) return false;
-    if(subString.length)
+    if(subArray.indexOf(key) > searchArray.lastIndexOf(key)){
+      return false;
+    }
   }
   return true;
 }
 
 
-// function letterFreq(letters){
-//   let letterCount = {};
-// }
+function letterFreq(letters){
+  const letterCount = {};
+  for(let letter of letters){
+    letterCount[letter] = letterCount[letter] + 1 || 1;
+  }
+  return letterCount;
+}
+
+isSubsequence("abc", "abracadabra"); //true
