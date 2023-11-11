0bf17b66db56ff6fa3b0540e0b8a27d3c169f88c
Jia Rong Gan <jiaronggan21@gmail.com>
2023-11-10 18:32:29 -0600

    completed twoArray

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,9 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+
+//** Takes in two positive integers return true if both set of integers
+// are the same, else return false.  */
+
+function sameFrequency(int1, int2) {
 }
+
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,20 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+//** Takes an array of keys and an array of values, return an object but
+// if the array with values is shorter than the array with keys, the
+// missing key's value will be set to null */
+
+function twoArrayObject(arrayOfKeys, arrayOfValues) {
+  const combineKeyAndVal = {};
+// looping through all the keys
+// matching them at their index
+// if index's value is undefining, null will be assigned
+
+  for (let i = 0; i < arrayOfKeys.length; i++){
+    if (arrayOfValues[i] === undefined) {
+       arrayOfValues[i] = null;
+    }
+    combineKeyAndVal[arrayOfKeys[i]] = arrayOfValues[i];
+  }
+  return combineKeyAndVal;
 }
