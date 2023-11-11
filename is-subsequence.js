7332fd7336b77dc29e685dbc0547b1d0ad502456
Max Shpungin <maxsdance@gmail.com>
2023-11-10 17:01:07 -0800

    work in progress

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,27 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write docstring
-function isSubsequence() {
+/** takes a target subString and a searchString, returns true if subString
+ *  is found if searchString, else returns false
+ */
+function isSubsequence(subString,searchString) {
+  let left = 0;
+  let right = 0;
+
+  while(right < searchString.length){ // while(subString.length < searchString[left])
+    if(subString[left] !== searchString[right]){
+      right++;
+    }else{
+      left++;
+      right++;
+    }
+    // if(right === searchString.length-1) return false;
+    if(subString.length)
+  }
+  return true;
 }
+
+
+// function letterFreq(letters){
+//   let letterCount = {};
+// }
--- same-frequency.js
+++ same-frequency.js
@@ -5,5 +5,24 @@
 // are the same, else return false.  */
 
 function sameFrequency(int1, int2) {
+  let int1Str = countDigits(int1);
+  let int2Str = countDigits(int2);
+
+  if(int1Str.length !== int2Str.length) return false;
+
+  for(let key in int1Str){
+    if(int1Str[key] !== int2Str[key]) return false;
+  }
+  return true;
 }
 
+function countDigits(num){
+  const numStr = num.toString();
+  console.log(numStr);
+  let counter = {};
+
+  for (let digit of numStr){
+    counter[digit] = counter[digit] + 1 || 1;
+  }
+  return counter;
+}
