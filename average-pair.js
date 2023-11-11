afcd617229132e24c2f0e438396296832a51d76b
Max Shpungin <maxsdance@gmail.com>
2023-11-10 16:02:28 -0800

    solved average pair

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,32 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+
+/** Takes an array of nums and an average target Number
+ *  returns true if there is a pair average that matches target
+ *  otherwise returns false
+ */
+function averagePair(nums,avgTarget) {
+  let left = 0;
+  let right = nums.length-1;
+  let avgCurrent;
+  // check if value at left/right index avg === avg Target
+  // if value is too high, decrease right
+  //  otherwise increase left
+
+  while(left < right){
+    avgCurrent = (nums[left]+nums[right])/2;
+    if(avgCurrent === avgTarget){
+      return true;
+    }
+    if(avgCurrent > avgTarget){
+      right--;
+    }
+      else{
+        left++;
+      }
+  }
+
+  return false;
 }
 
